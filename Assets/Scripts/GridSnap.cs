7e85c8fbe9f033a12e5e0e1d59dd66814be70115
EngWeiYang <engwy05@gmail.com>
2023-11-27 16:32:05 +0800

    Updated drag and drop
    - Added easier boundary setup

--- Assets/Scripts/GridSnap.cs
+++ Assets/Scripts/GridSnap.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class GridSnap : MonoBehaviour
 {
     public float gridSize = 1f;
+    public int minX, maxX;
+    public int minZ, maxZ;
+
+    private float snappedX;
+    private float snappedZ;
+
+    private float clampedX;
+    private float clampedZ;
+
+    private BoxCollider objSize;
+    public BoxCollider boundingCollider;
+
+    private void Start()
+    {
+        objSize = GetComponent<BoxCollider>();
+    }
 
     void Update()
     {
@@ -12,10 +28,26 @@ public class GridSnap : MonoBehaviour
         Vector3 currentPosition = transform.position;
 
         // Snap to the grid by rounding to the nearest gridSize
-        float snappedX = Mathf.Round(currentPosition.x / gridSize) * gridSize;
-        float snappedZ = Mathf.Round(currentPosition.z / gridSize) * gridSize;
+        snappedX = Mathf.Round(currentPosition.x / gridSize) * gridSize;
+        snappedZ = Mathf.Round(currentPosition.z / gridSize) * gridSize;
+
+        //clampedX = Mathf.Clamp(snappedX, minX, maxX);
+        //clampedZ = Mathf.Clamp(snappedZ, minZ, maxZ);
+
+        Bounds bounds = boundingCollider.bounds;
+
+        if (objSize.size.x % 2 == 0)
+        {
+            clampedX = Mathf.Clamp(snappedX, bounds.min.x + 0.5f, bounds.max.x - 1.5f);
+            clampedZ = Mathf.Clamp(snappedZ, bounds.min.z + 0.5f, bounds.max.z - 0.5f);
+        }
+        else
+        {
+            clampedX = Mathf.Clamp(snappedX, bounds.min.x + 0.5f, bounds.max.x - 0.5f);
+            clampedZ = Mathf.Clamp(snappedZ, bounds.min.z + 0.5f, bounds.max.z - 0.5f);
+        }
 
         // Update the object's position
-        transform.position = new Vector3(snappedX, 0f, snappedZ);
+        transform.position = new Vector3(clampedX, 0f, clampedZ);
     }
 }
