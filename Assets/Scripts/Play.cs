02d7fddcbcc771ff4bd3ec2f5d28eaafb575aac4
LYilng <2201856H@student.tp.edu.sg>
2023-12-02 16:59:57 +0800

    Added main menu placeholders

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        Debug.Log("Loading level 1");
+        //SceneManager.LoadScene("Level01");
+    }
+
+    // Function to quit the game
+    public void QuitGame()
+    {
+        Debug.Log("U QUITed");
+        Application.Quit();
+    }
+}
